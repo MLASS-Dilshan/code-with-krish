d8f380447fcea6ce7cea130b0990e4af34ab8ec7
MLASS-Dilshan <sachira.sandeepa14@gmail.com>
2025-02-26 22:58:15 +0530

    asc and dece

--- api-server/index.js
+++ api-server/index.js
@@ -1,57 +1,73 @@
-const express = require('express')
+const express = require("express");
 
-const app = new express()
-const port = 3000
+const app = new express();
+const port = 3000;
 // const greeting = {message: "Hello Node"}
 
-app.get('/number/max', (req, res) => {
-    const num1 = parseFloat(req.query.num1)
-    const num2 = parseFloat(req.query.num2)
+// Maximum number
+app.get("/number/max", (req, res) => {
+  const num1 = parseFloat(req.query.num1);
+  const num2 = parseFloat(req.query.num2);
 
-    const a = require('../util')
+  const a = require("../util");
 
-    const maxNumber = a.maxNumber(num1,num2)
+  const maxNumber = a.maxNumber(num1, num2);
 
-    res.json(maxNumber)
-})
+  res.json(maxNumber);
+});
 
-app.get('/numbers/avg', (req, res) => {
-    // const num = req.query
-    const num = req.query.num ? req.query.num.split(',').map(Number) : [];
+// Minimum Number
+app.get("/number/min", (req, res) => {
+  const num1 = parseFloat(req.query.num1);
+  const num2 = parseFloat(req.query.num2);
 
-    console.log(num)
-    const h = require('../util')
+  const f = require("../util");
 
-    const averageArr = h.average(num)
+  const minNumber = f.minNumber(num1, num2);
 
-    console.log(averageArr)
-    res.json(averageArr)
+  res.json(minNumber);
+});
 
+// Average
+app.get("/numbers/avg", (req, res) => {
+  // const num = req.query
+  const num = req.query.num ? req.query.num.split(",").map(Number) : [];
 
-})
+  console.log(num);
+  const h = require("../util");
 
-app.get('/number/min', (req, res) => {
+  const averageArr = h.average(num);
 
-    const num1 = parseFloat(req.query.num1)
-    const num2 = parseFloat(req.query.num2)
-
-    const f = require('../util')
-
-    const minNumber = f.minNumber(num1, num2)
-
-    // if (isNaN(num1) || isNaN(num2)) {
-    //     console.log('this is not a number enter a correct number')
-    //     res.json("both values must be numbers")
-    // } else {
-    //     res.json({min : num1 < num2 ? num1 : num2})
-    // }
-
-    // res.json(greeting)
-    // res.json(Math.min(num1, num2))
-    res.json(minNumber)
-   
-})
+  console.log(averageArr);
+  res.json(averageArr);
+});
 
 app.listen(port, () => {
-    console.log(`server is running on ${port}`)
-})
+  console.log(`server is running on ${port}`);
+});
+
+// Ascending order
+
+app.get("/number/sort/asc", (req, res) => {
+  const num = req.query.num ? req.query.num.split(",").map(Number) : [];
+
+  const b = require("../util");
+
+  const sortedAscending = b.ascendingOrder(num);
+
+  console.log(b.ascendingOrder(num));
+  res.json(sortedAscending);
+});
+
+// descending order
+
+app.get("/number/sort/des", (req, res) => {
+  const num = req.query.num ? req.query.num.split(",").map(Number) : [];
+
+  const b = require("../util");
+
+  const sortedDescending = b.descendingOrder(num);
+
+  console.log(sortedDescending);
+  res.json(sortedDescending);
+});
--- util.js
+++ util.js
@@ -1,52 +1,113 @@
+// Minimum number function
 const minNumber = (num1, num2) => {
-    // return Math.min(num1, num2)
+  // return Math.min(num1, num2)
 
-    if (isNaN(num1) || isNaN(num2)) {
-        return {
-            status : 400,
-            data : {
-                error: `Both should be numbers`
-            }
-        }
-    } else {
-        return {
-            status : 200,
-            data : {
-                Max: Math.min(num1, num2)
-            }
-        }
-    }
-    
-}
+  if (isNaN(num1) || isNaN(num2)) {
+    return {
+      status: 400,
+      data: {
+        error: `Both should be numbers`,
+      },
+    };
+  } else {
+    return {
+      status: 200,
+      data: {
+        Max: Math.min(num1, num2),
+      },
+    };
+  }
+};
 
+// maximum number function
 const maxNumber = (num1, num2) => {
-    // return Math.max(num1, num2)
-    if (isNaN(num1) || isNaN(num2)) {
-        return {
-            status : 400,
-            data : {
-                error: `Both should be numbers`
-            }
-        }
-    } else {
-        return {
-            status : 200,
-            data : {
-                Max: Math.max(num1, num2)
-            }
-        }
-    }
-}
+  // return Math.max(num1, num2)
+  if (isNaN(num1) || isNaN(num2)) {
+    return {
+      status: 400,
+      data: {
+        error: `Both should be numbers`,
+      },
+    };
+  } else {
+    return {
+      status: 200,
+      data: {
+        Max: Math.max(num1, num2),
+      },
+    };
+  }
+};
 
+// average function
 const average = (num) => {
-    let sum = 0
-    for (let i = 0 ; i < num.length; i++ ) {
-        sum += num[i]
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum += num[i];
+  }
+
+  return sum / num.length;
+};
+
+// ascending function
+
+const ascendingOrder = (num) => {
+  if (num.some(isNaN)) {
+    return {
+      status: 400,
+      data: {
+        error: `Invalid input`,
+      },
+    };
+  } else {
+    let a = num.length;
+
+    for (let i = 0; i < a - 1; i++) {
+      for (let j = 0; j < a - i - 1; j++) {
+        if (num[j] > num[j + 1]) {
+          let temp = num[j];
+          num[j] = num[j + 1];
+          num[j + 1] = temp;
+        }
+      }
     }
 
-    return sum / num.length
-}
+    return num;
+  }
+};
+
+// descending function
+const descendingOrder = (num) => {
+  if (num.some(isNaN)) {
+    return {
+      status: 400,
+      data: {
+        error: `Invalid input`,
+      },
+    };
+  } else {
+    let a = num.length;
+
+    for (let i = 0; i < a - 1; i++) {
+      for (let j = 0; j < a - i - 1; j++) {
+        if (num[j] < num[j + 1]) {
+          let temp = num[j];
+          num[j] = num[j + 1];
+          num[j + 1] = temp;
+        }
+      }
+    }
+
+    return num;
+  }
+};
 
 // minNumber(num1, num2)
 
-module.exports = {minNumber, maxNumber, average}
+module.exports = {
+  minNumber,
+  maxNumber,
+  average,
+  ascendingOrder,
+  descendingOrder,
+};
